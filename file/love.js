1d8c6d2898d534a20f13131ddb0efced2dcbf133
Mehedi-Hasan11 <mhasan95211@gmail.com>
2025-09-14 20:43:17 +0600

    Correction

--- file/love.js
+++ file/love.js
@@ -168,8 +168,8 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("Touch Here ) ", 30, -5);
+            ctx.fillText("MY TULTUL PAKHI 🕊️ !", 28, 10);
             ctx.restore();
         },
         clear: function() {
